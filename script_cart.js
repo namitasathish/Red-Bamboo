2d76abf6c360333b0d4ab6be3a020d4260afef4f
Namita Sathish <namitasathish1@gmail.com>
2025-01-06 14:53:47 +0530

    Update script_cart.js

--- script_cart.js
+++ script_cart.js
@@ -1,9 +1,17 @@
 
 const menuItems = [
-    { id: 1, name: 'Soup', price: 10, img: 'assets/soup.jpg' },
-    { id: 2, name: 'Sushi', price: 15, img: 'assets/sushi.jpeg' },
-    { id: 3, name: 'Curry', price: 12, img: 'assets/curry.jpg' },
-    { id: 4, name: 'Mochi', price: 8, img: 'assets/mochi.avif' }
+    { id: 1, name: 'Soup', price: 22, img: 'assets/soup.jpg' },
+    { id: 2, name: 'Sushi', price: 28, img: 'assets/sushi.jpeg' },
+    { id: 3, name: 'Curry and Rice', price: 25, img: 'assets/curry.jpg' },
+    { id: 4, name: 'Mochi', price: 8, img: 'assets/mochi.avif' },
+    { id: 1, name: 'Dumplings', price: 22, img: 'assets/dump.jpg' },
+    { id: 2, name: 'Pho', price: 18, img: 'assets/pho.webp' },
+    { id: 3, name: 'Satay and Rice', price: 20, img: 'assets/satay.jpg' },
+    { id: 4, name: 'Peking Duck', price: 30, img: 'assets/duck.jpg' },
+    { id: 5, name: 'Boba Tea', price: 10, img: 'assets/boba.png' },
+    { id: 6, name: 'Vietnamese Coffee', price: 12, img: 'assets/viet.png' },
+    { id: 7, name: 'Soy Milk', price: 7, img: 'assets/soyy.jpg' },
+    { id: 8, name: 'Tender Coconut', price: 5, img: 'assets/coco.jpg' }
 ];
 
 
